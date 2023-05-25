8cd76942f57c30ba2543fa3dd353171e900e1381
jghan <gojkhan13@gmail.com>
2023-05-25 18:05:19 +0900

    jpa detatch, flush

---
+++ src/main/java/hellojpa/JpaMain_detatch.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain_detatch {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = em.find(Member.class, 200L);
+            member.setName("AAAA");
+            em.detach(member); //준영속 전환
+
+            System.out.println("============================");
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
