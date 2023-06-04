f6d48597f8bcda8faeddf2f6099bae5bbc1c1831
jghan <gojkhan13@gmail.com>
2023-06-05 00:26:52 +0900

    7-상속관계매핑

--- src/main/java/hellojpa/domain/Item.java
+++ src/main/java/hellojpa/domain/Item.java
@@ -9,12 +9,16 @@ import java.util.List;
 
 @Entity
 @Getter @Setter
-public class Item {
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@DiscriminatorColumn
+public abstract class Item {
     @Id
     @GeneratedValue
     private Long id;
 
     private String name;
+
+    private int price;
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
